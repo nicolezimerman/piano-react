60cb248d5a0e97352c24fdfc6b90957651ec8e83
nzimerman <nzimerman@glamit.com.ar>
2020-08-22 21:04:40 -0300

    Primer version del piano

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ estaSonando, setEstaSonando ] = useState("");
+
+  const notas = [
+    {
+      nombre: "DO",
+      link: require("./notas/do.wav"),
+      tieneSostenido: true
+    },
+    {
+      nombre: "RE",
+      link: require("./notas/re.wav"),
+      tieneSostenido: true
+    },
+    {
+      nombre: "MI",
+      link: require("./notas/mi.wav")
+    },
+    {
+      nombre: "FA",
+      link: require("./notas/fa.wav"),
+      tieneSostenido: true
+    },
+    {
+      nombre: "SOL",
+      link: require("./notas/sol.wav"),
+      tieneSostenido: true
+    },
+    {
+      nombre: "LA",
+      link: require("./notas/la.wav"),
+      tieneSostenido: true
+    },
+    {
+      nombre: "SI",
+      link: require("./notas/si.wav")
+    }
+  ];
+
+  const handleClick = (nota) => {
+    setEstaSonando(nota.nombre)
+    const sonido = new Audio(nota.link)
+    sonido.play();
+    console.log(nota)
+    setTimeout(() => {
+      setEstaSonando(null)
+    }, 300)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="titulo">
+        <h1>Piano</h1>
+        <h2>Hecho con amor por Nicky</h2>
+      </div>
+      <div className="contenedor">
+        {
+          notas.map(nota => {
+            return(
+              <div 
+                className={`nota ${estaSonando === nota.nombre && 'estaSonando'}`} 
+                onClick={() => handleClick(nota)}>
+                  { nota.tieneSostenido && <div className="negra"></div> }
+
+              </div>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
