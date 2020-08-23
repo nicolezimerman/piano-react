27a594f64db8158352c795f8d321dbb779c1ae14
nzimerman <nzimerman@glamit.com.ar>
2020-08-23 10:47:11 -0300

    Updates

---
+++ precache-manifest.0d52ec89d0b0c088456b36979748d204.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108f347eba0315388d5286e810bab8f3",
+    "url": "/piano-react/index.html"
+  },
+  {
+    "revision": "c89a766fcb6c3f1a139e",
+    "url": "/piano-react/static/css/main.46d4a6c8.chunk.css"
+  },
+  {
+    "revision": "27c2650df846a92e020f",
+    "url": "/piano-react/static/js/2.be42e844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/piano-react/static/js/2.be42e844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89a766fcb6c3f1a139e",
+    "url": "/piano-react/static/js/main.551343d4.chunk.js"
+  },
+  {
+    "revision": "cfa43907fde155773558",
+    "url": "/piano-react/static/js/runtime-main.085f8a8b.js"
+  },
+  {
+    "revision": "18fd3f6f1a6879d60b471eb4828bd890",
+    "url": "/piano-react/static/media/do.18fd3f6f.wav"
+  },
+  {
+    "revision": "a787534ad7a68707ce5c7657c0a26985",
+    "url": "/piano-react/static/media/fa.a787534a.wav"
+  },
+  {
+    "revision": "10af03f3aa8a23a3e29cab5bb9f7d48f",
+    "url": "/piano-react/static/media/la.10af03f3.wav"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/piano-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f26c778a049229d1c7a2edfb789b5fd",
+    "url": "/piano-react/static/media/mi.1f26c778.wav"
+  },
+  {
+    "revision": "217b943a565a4c60e452aac2852fd0e9",
+    "url": "/piano-react/static/media/re.217b943a.wav"
+  },
+  {
+    "revision": "70924435d531fe3a2c59d6596733c5cc",
+    "url": "/piano-react/static/media/si.70924435.wav"
+  },
+  {
+    "revision": "785a33d2df8ac21f71be64df8f8f579a",
+    "url": "/piano-react/static/media/sol.785a33d2.wav"
+  }
+]);
