8d2988577b9aaf7a7fbf7ffff562991fb5da1b03
nzimerman <nzimerman@glamit.com.ar>
2020-08-27 22:56:34 -0300

    Agrego funcion para tocar con teclado--primer version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
 
   const [ estaSonando, setEstaSonando ] = useState("");
+
+
 
   const notas = [
     {
       nombre: "DO",
       link: require("./notas/do.wav"),
       tieneSostenido: true,
-      color: "red"
+      color: "red",
+      key: "a",
     },
     {
       nombre: "RE",
@@ -49,6 +53,15 @@ function App() {
     }
   ];
 
+
+  const pressDo = useKeyPress('a',notas[0]);
+  const pressRe = useKeyPress('s',notas[1]);
+  const pressMi = useKeyPress('d',notas[2]);
+  const pressFa = useKeyPress('f',notas[3]);
+  const pressSol = useKeyPress('g',notas[4]);
+  const pressLa = useKeyPress('h',notas[5]);
+  const pressSi = useKeyPress('j',notas[6]);
+
   const handleClick = (nota) => {
     setEstaSonando(nota.nombre)
     const sonido = new Audio(nota.link)
@@ -58,6 +71,41 @@ function App() {
       setEstaSonando(null)
     }, 300)
   };
+
+  // Hook
+  function useKeyPress(targetKey,nota) {
+    // State for keeping track of whether key is pressed
+    const [keyPressed, setKeyPressed] = useState(false);
+
+    // If pressed key is our target key then set to true
+    function downHandler({ key }) {
+      if (key === targetKey) {
+        setKeyPressed(true);
+        handleClick(nota);
+      }
+    }
+
+    // If released key is our target key then set to false
+    const upHandler = ({ key }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    };
+
+    // Add event listeners
+    useEffect(() => {
+      window.addEventListener('keydown', downHandler);
+      window.addEventListener('keyup', upHandler);
+      // Remove event listeners on cleanup
+      return () => {
+        window.removeEventListener('keydown', downHandler);
+        window.removeEventListener('keyup', upHandler);
+      };
+    }, []); // Empty array ensures that effect is only run on mount and unmount
+
+    return keyPressed;
+  }
+
 
   return (
     <div className="App">
@@ -70,13 +118,20 @@ function App() {
             return(
               <div 
                 className={`nota ${estaSonando === nota.nombre && 'estaSonando'}`} 
-                onClick={() => handleClick(nota)} 
+                onClick={() => handleClick(nota)}
                 style={ estaSonando === nota.nombre ? { backgroundColor:`${nota.color}`} : {}}>
                   { nota.tieneSostenido && <div className="negra"></div> }
               </div>
             )
           })
         }
+        {pressDo}
+        {pressRe}      
+        {pressMi}      
+        {pressFa}
+        {pressSol}      
+        {pressLa}      
+        {pressSi}        
       </div>
       <div className="footer">
         <h2>Hecho con  por Nicky Zimerman</h2>
@@ -84,5 +139,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
